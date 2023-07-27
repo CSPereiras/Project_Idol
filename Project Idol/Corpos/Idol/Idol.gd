dfe6906364b8e032942a22f1db109be72114adfc
Fidel C.S. Pereira <cspereira2000@gmail.com>
2023-07-27 13:18:51 -0300

    Mais uma atualização

--- Project Idol/Corpos/Idol/Idol.gd
+++ Project Idol/Corpos/Idol/Idol.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 400
+const SPEED = 300
 const JUMP_VELOCITY = 13 #-500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -10,6 +10,8 @@ var pulo = 0
 var imunidade = false
 var impulsoDano = false
 var impulsoPulo = true
+var podePular = true
+var colisao
 
 signal morreu
 signal tomouDano
@@ -20,15 +22,21 @@ func _physics_process(_delta):
 		velocity.y += gravity * _delta
 	
 	# Handle Jump.
-	if Input.is_action_pressed("accept") and is_on_floor() and !impulsoDano and impulsoPulo:
+	if Input.is_action_pressed("accept") and is_on_floor() and !impulsoDano and impulsoPulo and podePular:
 		jump()
-		verificaTeto()
 		$TimerImpulsoPulo.start()
+		podePular = false
 	
 	if Input.is_action_just_released("accept") or !impulsoPulo:
 		jump_cut()
 		if is_on_floor():
 			impulsoPulo = true
+	
+	if is_on_ceiling():
+		impulsoPulo = false
+	
+	if !Input.is_action_pressed("accept") and is_on_floor():
+		podePular = true
 	
 	self.global_position.y -= pulo
 	
@@ -58,19 +66,17 @@ func jump():
 func jump_cut():
 	pulo = 0
 
-func verificaTeto():
-	print(is_on_ceiling())
-	if is_on_ceiling():
-		impulsoPulo =false
-
 func _on_patrulheiro_body_entered(body):
-	if(body.name == "Idol"):
-		impulsoDano = true
-		$TimerDano.start()
-		vidas -= 1
-		imunidade = true
-		$TimerImunidade.start()
-		emit_signal("tomouDano")
+	if(body.name == "Idol" and !body.imunidade):
+		dano()
+
+func dano():
+	impulsoDano = true
+	$TimerDano.start()
+	vidas -= 1
+	imunidade = true
+	$TimerImunidade.start()
+	emit_signal("tomouDano")
 
 func _on_timer_timeout():
 	impulsoDano = false
@@ -80,3 +86,4 @@ func _on_timer_imunidade_timeout():
 
 func _on_timer_impulso_pulo_timeout():
 	impulsoPulo = false
+
--- Project Idol/Corpos/Inimgos/Patrulheiro/patrulheiro.gd
+++ Project Idol/Corpos/Inimgos/Patrulheiro/patrulheiro.gd
@@ -7,6 +7,3 @@ var direction = -1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	global_position.x += direction * SPEED
-	
-
-
